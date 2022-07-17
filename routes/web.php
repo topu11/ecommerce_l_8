7f2e4e3d07db3ba8521a43656852365b48fc5152
Touhidul-MysoftHeaven <touhidul.developer.2022@gmail.com>
2022-07-17 16:02:19 +0600

    livewire pages

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,9 @@ use App\Http\Livewire\CartComponent;
 use App\Http\Livewire\HomeComponent;
 use App\Http\Livewire\ShopComponent;
 use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AboutUsComponent;
 use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactUsComponent;
 
 /*
 |--------------------------------------------------------------------------
@@ -24,3 +26,5 @@ Route::get('/',HomeComponent::class);
 Route::get('/shop',ShopComponent::class);
 Route::get('/cart',CartComponent::class);
 Route::get('/checkout',CheckoutComponent::class);
+Route::get('/about-us',AboutUsComponent::class);
+Route::get('/contact-us',ContactUsComponent::class);
