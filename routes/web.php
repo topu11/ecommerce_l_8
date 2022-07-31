c850d825a322ae5391509deccf9a7858e3522d9c
Fahim-Raiyan <fahimraiyan2803@gmail.com>
2022-07-31 20:54:00 +0600

    raiyan_home

---
+++ routes/web.php
@@ -0,0 +1,40 @@
+<?php
+
+use App\Http\Livewire\CartComponent;
+use App\Http\Livewire\HomeComponent;
+use App\Http\Livewire\ShopComponent;
+use Illuminate\Support\Facades\Route;
+use App\Http\Livewire\AboutUsComponent;
+use App\Http\Livewire\CheckoutComponent;
+use App\Http\Livewire\ContactUsComponent;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+ */
+
+
+
+
+Route::get('/',HomeComponent::class);
+Route::get('/shop',ShopComponent::class);
+Route::get('/cart',CartComponent::class);
+Route::get('/checkout',CheckoutComponent::class);
+Route::get('/about-us',AboutUsComponent::class);
+Route::get('/contact-us',ContactUsComponent::class);
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
